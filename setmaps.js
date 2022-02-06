46973a5114d70f7a961ed69c7b8fe2858930ac85
Bisma Bendesa <bismabendesajaya@gmail.com>
2022-02-06 19:30:53 +0800

    Working with sets

---
+++ setmaps.js
@@ -0,0 +1,13 @@
+// working with sets
+// used to work with an unique data (id)
+// sets cannot have the same value, cannot duplicate and not index based
+// const ids = new Set(['hi', 'from', 'sets']);
+// ids.add(2);
+// if (ids.has('hi')){
+//   ids.delete('hi');
+// }
+// console.log(ids);
+
+// for (const entry of ids.values()){
+//   console.log(entry);
+// }
